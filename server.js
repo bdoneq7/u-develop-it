2f32a8ddab6e19ed9edfd3cf62dbfbc9fb7f719d
Brian Done <briguy2078@gmail.com>
2022-01-03 21:26:32 -0500

    connected to sql database and queried candidates table

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const { process_params } = require('express/lib/router');
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,24 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// Connect to SQL Database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySql Username
+        user: 'root',
+        // Your MySql Password
+        password: '.C3MsZ%56eX8x*Sp',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// database query
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 // Default response for any other request (Not Found) 
